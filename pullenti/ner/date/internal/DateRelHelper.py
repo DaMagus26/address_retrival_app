12eeb1a569d43d016513980c976130b0aae0774a
Michael Ovakimian <mov.express17@gmail.com>
2024-04-25 00:37:39 +0300

    Added Pullenti (External library for NLP tasks)

---
+++ pullenti/ner/date/internal/DateRelHelper.py
@@ -0,0 +1,377 @@
+﻿# SDK Pullenti Address, version 4.23, march 2024. Copyright (c) 2013, Pullenti. All rights reserved.
+# Non-Commercial Freeware and Commercial Software.
+# This class is generated using the converter Unisharping (www.unisharping.ru) from Pullenti C# project.
+# The latest version of the code is available on the site www.pullenti.ru
+
+import typing
+import operator
+import datetime
+import io
+from pullenti.unisharp.Utils import Utils
+from pullenti.unisharp.Misc import RefOutArgWrapper
+
+from pullenti.ner.ReferentToken import ReferentToken
+from pullenti.ner.date.DatePointerType import DatePointerType
+from pullenti.ner.ProcessorService import ProcessorService
+from pullenti.ner.Referent import Referent
+from pullenti.ner.date.DateReferent import DateReferent
+from pullenti.ner.date.DateRangeReferent import DateRangeReferent
+from pullenti.ner.date.internal.DateExToken import DateExToken
+
+class DateRelHelper:
+    
+    @staticmethod
+    def create_referents(et : 'DateExToken') -> typing.List['ReferentToken']:
+        if (not et.is_diap or len(et.items_to) == 0): 
+            li = DateRelHelper.__create_refs(et.items_from)
+            if (li is None or len(li) == 0): 
+                return None
+            return li
+        li_fr = DateRelHelper.__create_refs(et.items_from)
+        li_to = DateRelHelper.__create_refs(et.items_to)
+        ra = DateRangeReferent()
+        if (len(li_fr) > 0): 
+            ra.date_from = Utils.asObjectOrNull(li_fr[0].tag, DateReferent)
+        if (len(li_to) > 0): 
+            ra.date_to = Utils.asObjectOrNull(li_to[0].tag, DateReferent)
+        res = list()
+        res.extend(li_fr)
+        res.extend(li_to)
+        res.append(ReferentToken(ra, et.begin_token, et.end_token))
+        if (len(res) == 0): 
+            return None
+        res[0].tag = (ra)
+        return res
+    
+    @staticmethod
+    def __create_refs(its : typing.List['DateExItemToken']) -> typing.List['ReferentToken']:
+        res = list()
+        own = None
+        i = 0
+        first_pass3304 = True
+        while True:
+            if first_pass3304: first_pass3304 = False
+            else: i += 1
+            if (not (i < len(its))): break
+            it = its[i]
+            d = DateReferent()
+            if (it.is_value_relate): 
+                d.is_relative = True
+            if (own is not None): 
+                d.higher = own
+            if (it.typ == DateExToken.DateExItemTokenType.DAY): 
+                d.day = it.value
+                if (it.is_last and ((it.value == 0 or it.value == -1)) and i > 0): 
+                    it0 = its[i - 1]
+                    day = 0
+                    if (it0.typ == DateExToken.DateExItemTokenType.MONTH and not it0.is_value_relate): 
+                        m = d.month
+                        if (((m == 1 or m == 3 or m == 5) or m == 7 or m == 8) or m == 10 or m == 12): 
+                            day = 31
+                        elif (m == 2): 
+                            day = 28
+                        elif (m > 0): 
+                            day = 30
+                    elif (it0.typ == DateExToken.DateExItemTokenType.QUARTAL and not it0.is_value_relate): 
+                        m = 1 + (((it0.value - 1)) * 4)
+                        dm = DateReferent()
+                        dm.month = m
+                        if (own is not None): 
+                            dm.higher = own
+                        res.append(ReferentToken(dm, it.begin_token, it.end_token))
+                        d.higher = dm
+                        own = d.higher
+                        if (((m == 1 or m == 3 or m == 5) or m == 7 or m == 8) or m == 10 or m == 12): 
+                            day = 31
+                        elif (m == 2): 
+                            day = 28
+                        elif (m > 0): 
+                            day = 30
+                    elif (it0.typ == DateExToken.DateExItemTokenType.YEAR): 
+                        dm = DateReferent()
+                        dm.month = 12
+                        if (own is not None): 
+                            dm.higher = own
+                        res.append(ReferentToken(dm, it.begin_token, it.end_token))
+                        d.higher = dm
+                        own = d.higher
+                        day = 31
+                    elif (it0.typ == DateExToken.DateExItemTokenType.CENTURY): 
+                        dy = DateReferent()
+                        dy.year = 99
+                        dy.is_relative = True
+                        if (own is not None): 
+                            dy.higher = own
+                        res.append(ReferentToken(dy, it.begin_token, it.end_token))
+                        own = dy
+                        dm = DateReferent()
+                        dm.month = 12
+                        dm.higher = own
+                        res.append(ReferentToken(dm, it.begin_token, it.end_token))
+                        d.higher = dm
+                        own = d.higher
+                        day = 31
+                    if ((day + it.value) > 0): 
+                        d.is_relative = False
+                        d.day = day + it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.DAYOFWEEK): 
+                d.day_of_week = it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.HOUR): 
+                d.hour = it.value
+                if (((i + 1) < len(its)) and its[i + 1].typ == DateExToken.DateExItemTokenType.MINUTE and not its[i + 1].is_value_relate): 
+                    d.minute = its[i + 1].value
+                    i += 1
+            elif (it.typ == DateExToken.DateExItemTokenType.MINUTE): 
+                d.minute = it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.MONTH): 
+                d.month = it.value
+                if (it.is_last and ((it.value == 0 or it.value == -1)) and i > 0): 
+                    it0 = its[i - 1]
+                    m = 0
+                    if (it0.typ == DateExToken.DateExItemTokenType.QUARTAL and not it0.is_value_relate): 
+                        m = (1 + (((it0.value - 1)) * 4) + it.value)
+                    elif (it0.typ == DateExToken.DateExItemTokenType.YEAR or it0.typ == DateExToken.DateExItemTokenType.DECADE or it0.typ == DateExToken.DateExItemTokenType.CENTURY): 
+                        m = (12 + it.value)
+                    if (m > 0): 
+                        d.is_relative = False
+                        d.month = m
+            elif (it.typ == DateExToken.DateExItemTokenType.QUARTAL): 
+                d.quartal = it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.SEASON): 
+                d.season = it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.WEEK): 
+                d.week = it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.HALFYEAR): 
+                d.halfyear = (2 if it.is_last else it.value)
+            elif (it.typ == DateExToken.DateExItemTokenType.YEAR): 
+                d.year = it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.CENTURY): 
+                d.century = it.value
+            elif (it.typ == DateExToken.DateExItemTokenType.DECADE): 
+                d.decade = it.value
+            else: 
+                continue
+            res.append(ReferentToken(d, it.begin_token, it.end_token))
+            own = d
+            it.src = d
+        if (len(res) > 0): 
+            res[0].tag = (own)
+        return res
+    
+    @staticmethod
+    def __create_date_ex(dr : 'DateReferent') -> typing.List['DateExItemToken']:
+        res = list()
+        while dr is not None: 
+            n = 0
+            for s in dr.slots: 
+                it = DateExToken.DateExItemToken._new1080(None, None, DateExToken.DateExItemTokenType.UNDEFINED)
+                if (dr.get_string_value(DateReferent.ATTR_ISRELATIVE) == "true"): 
+                    it.is_value_relate = True
+                if (s.type_name == DateReferent.ATTR_YEAR): 
+                    it.typ = DateExToken.DateExItemTokenType.YEAR
+                    wrapn1081 = RefOutArgWrapper(0)
+                    inoutres1082 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1081)
+                    n = wrapn1081.value
+                    if (inoutres1082): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_DECADE): 
+                    it.typ = DateExToken.DateExItemTokenType.DECADE
+                    wrapn1083 = RefOutArgWrapper(0)
+                    inoutres1084 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1083)
+                    n = wrapn1083.value
+                    if (inoutres1084): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_CENTURY): 
+                    it.typ = DateExToken.DateExItemTokenType.CENTURY
+                    wrapn1085 = RefOutArgWrapper(0)
+                    inoutres1086 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1085)
+                    n = wrapn1085.value
+                    if (inoutres1086): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_HALFYEAR): 
+                    it.typ = DateExToken.DateExItemTokenType.HALFYEAR
+                    wrapn1087 = RefOutArgWrapper(0)
+                    inoutres1088 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1087)
+                    n = wrapn1087.value
+                    if (inoutres1088): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_QUARTAL): 
+                    it.typ = DateExToken.DateExItemTokenType.QUARTAL
+                    wrapn1089 = RefOutArgWrapper(0)
+                    inoutres1090 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1089)
+                    n = wrapn1089.value
+                    if (inoutres1090): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_SEASON): 
+                    it.typ = DateExToken.DateExItemTokenType.SEASON
+                    wrapn1091 = RefOutArgWrapper(0)
+                    inoutres1092 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1091)
+                    n = wrapn1091.value
+                    if (inoutres1092): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_MONTH): 
+                    it.typ = DateExToken.DateExItemTokenType.MONTH
+                    wrapn1093 = RefOutArgWrapper(0)
+                    inoutres1094 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1093)
+                    n = wrapn1093.value
+                    if (inoutres1094): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_WEEK): 
+                    it.typ = DateExToken.DateExItemTokenType.WEEK
+                    wrapn1095 = RefOutArgWrapper(0)
+                    inoutres1096 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1095)
+                    n = wrapn1095.value
+                    if (inoutres1096): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_DAYOFWEEK): 
+                    it.typ = DateExToken.DateExItemTokenType.DAYOFWEEK
+                    wrapn1097 = RefOutArgWrapper(0)
+                    inoutres1098 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1097)
+                    n = wrapn1097.value
+                    if (inoutres1098): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_DAY): 
+                    it.typ = DateExToken.DateExItemTokenType.DAY
+                    wrapn1099 = RefOutArgWrapper(0)
+                    inoutres1100 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1099)
+                    n = wrapn1099.value
+                    if (inoutres1100): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_HOUR): 
+                    it.typ = DateExToken.DateExItemTokenType.HOUR
+                    wrapn1101 = RefOutArgWrapper(0)
+                    inoutres1102 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1101)
+                    n = wrapn1101.value
+                    if (inoutres1102): 
+                        it.value = n
+                elif (s.type_name == DateReferent.ATTR_MINUTE): 
+                    it.typ = DateExToken.DateExItemTokenType.MINUTE
+                    wrapn1103 = RefOutArgWrapper(0)
+                    inoutres1104 = Utils.tryParseInt(Utils.asObjectOrNull(s.value, str), wrapn1103)
+                    n = wrapn1103.value
+                    if (inoutres1104): 
+                        it.value = n
+                if (it.typ != DateExToken.DateExItemTokenType.UNDEFINED): 
+                    res.insert(0, it)
+            dr = dr.higher
+        # PYTHON: sort(key=attrgetter('typ'))
+        res.sort(key=operator.attrgetter('typ'))
+        return res
+    
+    @staticmethod
+    def calculate_date(dr : 'DateReferent', now : datetime.datetime, tense : int) -> datetime.datetime:
+        if (dr.pointer == DatePointerType.TODAY): 
+            return now
+        if (not dr.is_relative and dr.dt is not None): 
+            return dr.dt
+        det = DateExToken(None, None)
+        det.items_from = DateRelHelper.__create_date_ex(dr)
+        return det.get_date(now, tense)
+    
+    @staticmethod
+    def calculate_date_range(dr : 'DateReferent', now : datetime.datetime, from0_ : datetime.datetime, to : datetime.datetime, tense : int) -> bool:
+        if (dr.pointer == DatePointerType.TODAY): 
+            from0_.value = now
+            to.value = now
+            return True
+        if (not dr.is_relative and dr.dt is not None): 
+            to.value = dr.dt
+            from0_.value = to.value
+            return True
+        det = DateExToken(None, None)
+        det.items_from = DateRelHelper.__create_date_ex(dr)
+        inoutres1105 = det.get_dates(now, from0_, to, tense)
+        return inoutres1105
+    
+    @staticmethod
+    def calculate_date_range2(dr : 'DateRangeReferent', now : datetime.datetime, from0_ : datetime.datetime, to : datetime.datetime, tense : int) -> bool:
+        from0_.value = datetime.datetime.min
+        to.value = datetime.datetime.max
+        dt0 = None
+        dt1 = None
+        if (dr.date_from is None): 
+            if (dr.date_to is None): 
+                return False
+            wrapdt01106 = RefOutArgWrapper(None)
+            wrapdt11107 = RefOutArgWrapper(None)
+            inoutres1108 = DateRelHelper.calculate_date_range(dr.date_to, now, wrapdt01106, wrapdt11107, tense)
+            dt0 = wrapdt01106.value
+            dt1 = wrapdt11107.value
+            if (not inoutres1108): 
+                return False
+            to.value = dt1
+            return True
+        elif (dr.date_to is None): 
+            wrapdt01109 = RefOutArgWrapper(None)
+            wrapdt11110 = RefOutArgWrapper(None)
+            inoutres1111 = DateRelHelper.calculate_date_range(dr.date_from, now, wrapdt01109, wrapdt11110, tense)
+            dt0 = wrapdt01109.value
+            dt1 = wrapdt11110.value
+            if (not inoutres1111): 
+                return False
+            from0_.value = dt0
+            return True
+        wrapdt01115 = RefOutArgWrapper(None)
+        wrapdt11116 = RefOutArgWrapper(None)
+        inoutres1117 = DateRelHelper.calculate_date_range(dr.date_from, now, wrapdt01115, wrapdt11116, tense)
+        dt0 = wrapdt01115.value
+        dt1 = wrapdt11116.value
+        if (not inoutres1117): 
+            return False
+        from0_.value = dt0
+        dt2 = None
+        dt3 = None
+        wrapdt21112 = RefOutArgWrapper(None)
+        wrapdt31113 = RefOutArgWrapper(None)
+        inoutres1114 = DateRelHelper.calculate_date_range(dr.date_to, now, wrapdt21112, wrapdt31113, tense)
+        dt2 = wrapdt21112.value
+        dt3 = wrapdt31113.value
+        if (not inoutres1114): 
+            return False
+        to.value = dt3
+        return True
+    
+    @staticmethod
+    def append_to_string(dr : 'DateReferent', res : io.StringIO) -> None:
+        dt0 = None
+        dt1 = None
+        cur = (datetime.datetime.now() if ProcessorService.DEBUG_CURRENT_DATE_TIME is None else ProcessorService.DEBUG_CURRENT_DATE_TIME)
+        wrapdt01118 = RefOutArgWrapper(None)
+        wrapdt11119 = RefOutArgWrapper(None)
+        inoutres1120 = DateRelHelper.calculate_date_range(dr, cur, wrapdt01118, wrapdt11119, 0)
+        dt0 = wrapdt01118.value
+        dt1 = wrapdt11119.value
+        if (not inoutres1120): 
+            return
+        DateRelHelper.__append_dates(cur, dt0, dt1, res)
+    
+    @staticmethod
+    def append_to_string2(dr : 'DateRangeReferent', res : io.StringIO) -> None:
+        dt0 = None
+        dt1 = None
+        cur = (datetime.datetime.now() if ProcessorService.DEBUG_CURRENT_DATE_TIME is None else ProcessorService.DEBUG_CURRENT_DATE_TIME)
+        wrapdt01121 = RefOutArgWrapper(None)
+        wrapdt11122 = RefOutArgWrapper(None)
+        inoutres1123 = DateRelHelper.calculate_date_range2(dr, cur, wrapdt01121, wrapdt11122, 0)
+        dt0 = wrapdt01121.value
+        dt1 = wrapdt11122.value
+        if (not inoutres1123): 
+            return
+        DateRelHelper.__append_dates(cur, dt0, dt1, res)
+    
+    @staticmethod
+    def __append_dates(cur : datetime.datetime, dt0 : datetime.datetime, dt1 : datetime.datetime, res : io.StringIO) -> None:
+        mon0 = dt0.month
+        print(" ({0}.{1}.{2}".format(dt0.year, "{:02d}".format(mon0), "{:02d}".format(dt0.day)), end="", file=res, flush=True)
+        if (dt0.hour > 0 or dt0.minute > 0): 
+            print(" {0}:{1}".format("{:02d}".format(dt0.hour), "{:02d}".format(dt0.minute)), end="", file=res, flush=True)
+        if (dt0 != dt1): 
+            mon1 = dt1.month
+            print("-{0}.{1}.{2}".format(dt1.year, "{:02d}".format(mon1), "{:02d}".format(dt1.day)), end="", file=res, flush=True)
+            if (dt1.hour > 0 or dt1.minute > 0): 
+                print(" {0}:{1}".format("{:02d}".format(dt1.hour), "{:02d}".format(dt1.minute)), end="", file=res, flush=True)
+        monc = cur.month
+        print(" отн. {0}.{1}.{2}".format(cur.year, "{:02d}".format(monc), "{:02d}".format(cur.day)), end="", file=res, flush=True)
+        if (cur.hour > 0 or cur.minute > 0): 
+            print(" {0}:{1}".format("{:02d}".format(cur.hour), "{:02d}".format(cur.minute)), end="", file=res, flush=True)
+        print(")", end="", file=res)
---
+++ pullenti/ner/geo/internal/TerrTermin.py
@@ -0,0 +1,241 @@
+﻿# SDK Pullenti Address, version 4.23, march 2024. Copyright (c) 2013, Pullenti. All rights reserved.
+# Non-Commercial Freeware and Commercial Software.
+# This class is generated using the converter Unisharping (www.unisharping.ru) from Pullenti C# project.
+# The latest version of the code is available on the site www.pullenti.ru
+
+
+from pullenti.ner.core.Termin import Termin
+
+class TerrTermin(Termin):
+    
+    def __init__(self, source : str, lang_ : 'MorphLang'=None) -> None:
+        super().__init__(None, lang_, False)
+        self.is_state = False
+        self.is_region = False
+        self.is_adjective = False
+        self.is_always_prefix = False
+        self.is_doubt = False
+        self.is_moscow_region = False
+        self.is_strong = False
+        self.is_specific_prefix = False
+        self.is_sovet = False
+        self.init_by_normal_text(source, lang_)
+    
+    @staticmethod
+    def _new1550(_arg1 : str, _arg2 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.gender = _arg2
+        return res
+    
+    @staticmethod
+    def _new1551(_arg1 : str, _arg2 : 'MorphLang', _arg3 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.gender = _arg3
+        return res
+    
+    @staticmethod
+    def _new1552(_arg1 : str, _arg2 : bool, _arg3 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_state = _arg2
+        res.gender = _arg3
+        return res
+    
+    @staticmethod
+    def _new1553(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_state = _arg3
+        res.gender = _arg4
+        return res
+    
+    @staticmethod
+    def _new1555(_arg1 : str, _arg2 : bool, _arg3 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_state = _arg2
+        res.is_doubt = _arg3
+        return res
+    
+    @staticmethod
+    def _new1556(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_state = _arg3
+        res.is_doubt = _arg4
+        return res
+    
+    @staticmethod
+    def _new1557(_arg1 : str, _arg2 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_state = _arg2
+        return res
+    
+    @staticmethod
+    def _new1558(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_state = _arg3
+        return res
+    
+    @staticmethod
+    def _new1559(_arg1 : str, _arg2 : bool, _arg3 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_state = _arg2
+        res.is_adjective = _arg3
+        return res
+    
+    @staticmethod
+    def _new1560(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_state = _arg3
+        res.is_adjective = _arg4
+        return res
+    
+    @staticmethod
+    def _new1561(_arg1 : str, _arg2 : bool, _arg3 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.gender = _arg3
+        return res
+    
+    @staticmethod
+    def _new1563(_arg1 : str, _arg2 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        return res
+    
+    @staticmethod
+    def _new1564(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_region = _arg3
+        res.gender = _arg4
+        return res
+    
+    @staticmethod
+    def _new1565(_arg1 : str, _arg2 : bool, _arg3 : str) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.acronym = _arg3
+        return res
+    
+    @staticmethod
+    def _new1566(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : str) -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_region = _arg3
+        res.acronym = _arg4
+        return res
+    
+    @staticmethod
+    def _new1571(_arg1 : str, _arg2 : bool, _arg3 : bool, _arg4 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.is_always_prefix = _arg3
+        res.gender = _arg4
+        return res
+    
+    @staticmethod
+    def _new1575(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : bool, _arg5 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_region = _arg3
+        res.is_always_prefix = _arg4
+        res.gender = _arg5
+        return res
+    
+    @staticmethod
+    def _new1579(_arg1 : str, _arg2 : bool, _arg3 : 'MorphGender', _arg4 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.gender = _arg3
+        res.is_always_prefix = _arg4
+        return res
+    
+    @staticmethod
+    def _new1580(_arg1 : str, _arg2 : bool, _arg3 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.is_always_prefix = _arg3
+        return res
+    
+    @staticmethod
+    def _new1585(_arg1 : str, _arg2 : bool, _arg3 : 'MorphGender', _arg4 : str) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.gender = _arg3
+        res.acronym = _arg4
+        return res
+    
+    @staticmethod
+    def _new1587(_arg1 : str, _arg2 : bool, _arg3 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.is_strong = _arg3
+        return res
+    
+    @staticmethod
+    def _new1590(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_region = _arg3
+        res.is_strong = _arg4
+        return res
+    
+    @staticmethod
+    def _new1594(_arg1 : str, _arg2 : str, _arg3 : bool, _arg4 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.canonic_text = _arg2
+        res.is_sovet = _arg3
+        res.gender = _arg4
+        return res
+    
+    @staticmethod
+    def _new1597(_arg1 : str, _arg2 : bool, _arg3 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.is_adjective = _arg3
+        return res
+    
+    @staticmethod
+    def _new1598(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_region = _arg3
+        res.is_adjective = _arg4
+        return res
+    
+    @staticmethod
+    def _new1599(_arg1 : str, _arg2 : bool, _arg3 : bool, _arg4 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_region = _arg2
+        res.is_specific_prefix = _arg3
+        res.is_always_prefix = _arg4
+        return res
+    
+    @staticmethod
+    def _new1600(_arg1 : str, _arg2 : 'MorphLang', _arg3 : bool, _arg4 : bool, _arg5 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1, _arg2)
+        res.is_region = _arg3
+        res.is_specific_prefix = _arg4
+        res.is_always_prefix = _arg5
+        return res
+    
+    @staticmethod
+    def _new1601(_arg1 : str, _arg2 : str, _arg3 : 'MorphGender') -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.acronym = _arg2
+        res.gender = _arg3
+        return res
+    
+    @staticmethod
+    def _new1602(_arg1 : str, _arg2 : str, _arg3 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.acronym = _arg2
+        res.is_region = _arg3
+        return res
+    
+    @staticmethod
+    def _new1603(_arg1 : str, _arg2 : str, _arg3 : str, _arg4 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.canonic_text = _arg2
+        res.acronym = _arg3
+        res.is_region = _arg4
+        return res
+    
+    @staticmethod
+    def _new1604(_arg1 : str, _arg2 : bool) -> 'TerrTermin':
+        res = TerrTermin(_arg1)
+        res.is_moscow_region = _arg2
+        return res
